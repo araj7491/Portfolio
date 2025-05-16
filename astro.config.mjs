b0ed7d0ae4a4e9c89613096466bd1f38904cb2f8
Ankit <araj7491@gmail.com>
2025-05-16 07:27:03 +0530

    update portfolio website

---
+++ astro.config.mjs
@@ -0,0 +1,13 @@
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+import mdx from '@astrojs/mdx';
+
+export default defineConfig({
+  integrations: [
+    tailwind(),
+    react(),
+    mdx(),
+  ],
+  site: 'https://ankit-raj-portfolio.netlify.app',
+});
